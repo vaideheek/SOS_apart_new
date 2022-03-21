ceb04701da7b2d8e97701ca0a2f763e14b8b75d3
vaideheek <101187945+vaideheek@users.noreply.github.com>
2022-03-21 12:58:57 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/FXMLAnimalController.java
@@ -0,0 +1,15 @@
+package hu.unideb.inf;
+
+import javafx.fxml.FXML;
+
+public class FXMLAnimalController {
+    /*@FXML
+    private Label helloLabel;*/
+
+    @FXML
+    void handleButtonPushed(){
+        //helloLabel.setText("Button pushed");
+        System.out.println("Button Pushed");
+
+    }
+}
