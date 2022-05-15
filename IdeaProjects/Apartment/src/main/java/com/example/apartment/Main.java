21a8838fb0a8edf8bae0ea094b57c1c609c85fc7
vaideheek <101187945+vaideheek@users.noreply.github.com>
2022-05-15 11:04:50 +0200

    Update Main.java

--- IdeaProjects/Apartment/src/main/java/com/example/apartment/Main.java
+++ IdeaProjects/Apartment/src/main/java/com/example/apartment/Main.java
@@ -5,13 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login-page.fxml"));
-        primaryStage.setTitle("Login");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
